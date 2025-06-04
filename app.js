e878e080719290cd47fecd56c4cdc7ac716adff8
malathif <malathif@centizen.com>
2025-06-04 21:57:02 +0530

    msg

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 console.log("log");
+console.log("new log");
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
